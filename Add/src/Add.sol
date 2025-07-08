a43ef9802c65a207a10c093db9334aadf40fe1a9
filip <fbielejec@gmail.com>
2025-07-08 11:39:59 +0200

    EC Adder

---
+++ Add/src/Add.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Add {
+    function add(
+        uint Ax,
+        uint Ay,
+        uint Bx,
+        uint By
+    ) public view returns (uint256 Cx, uint Cy) {
+        bytes memory payload = abi.encode(Ax, Ay, Bx, By);
+
+        (bool _ok, bytes memory answer) = address(6).staticcall(payload);
+
+        (Cx, Cy) = abi.decode(answer, (uint, uint));
+    }
+}
